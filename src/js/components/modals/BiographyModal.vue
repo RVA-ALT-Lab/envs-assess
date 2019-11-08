2723d5d7345765e22f0a747497c9a88dbb91c74b
Jeff Everhart <Jeffeverhart383@gmail.com>
2019-11-08 15:10:48 -0500

    update biography modal to pull in student data

--- src/js/components/modals/BiographyModal.vue
+++ src/js/components/modals/BiographyModal.vue
@@ -7,22 +7,22 @@
   <div class="modal-dialog modal-dialog-slideout" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h2 class="modal-title" id="bioModalLabel">Student Name</h2>
+        <h2 class="modal-title" id="bioModalLabel">{{student.name}}</h2>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">X</span>
         </button>
 
       </div>
       <div class="modal-body">
-      	<img id="stu-photo" src="" alt="Student biography image.">
-      	<div id="stu-grad"></div>
-        <div id="stu-majors"></div>
-        <div id="stu-minors"></div>
+      	<img id="stu-photo" :src="student.pic" alt="Student biography image.">
+      	<div id="stu-grad">{{student.grad}}</div>
+        <div id="stu-majors">{{student.major}}</div>
+        <div id="stu-minors">{{student.minor}}</div>
         <h3>General Portfolio Data</h3>
-        <div id="stu-page-count"></div>
-        <div id="stu-post-count"></div>
-        <div id="stu-created"></div>
-        <div id="stu-last-update"></div>
+        <div id="stu-page-count">{{student.postCount}}</div>
+        <div id="stu-post-count">{{student.pageCount}}</div>
+        <div id="stu-created">{{student.created}}</div>
+        <div id="stu-last-update">{{student.lastUpdate}}</div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -34,6 +34,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'BiographyModal',
   data () {
@@ -41,6 +42,9 @@ export default {
     }
   },
   computed: {
+    ...mapGetters([
+      'student'
+    ])
   },
   methods: {
   }
