3f70ebb9ef6dcf4ad97205a6104f5d0a419e14b8
Jeff Everhart <Jeffeverhart383@gmail.com>
2019-11-13 14:12:00 -0500

    display category on post card

--- src/js/components/Post.vue
+++ src/js/components/Post.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
    <div class="card">
-     <div class="card-header">
-       {{post.id}}
-     </div>
       <div class="card-body">
-        <h5 class="card-title">{{post.title.rendered}}</h5>
+        <h5 class="card-title">{{post.title.rendered}} ({{post.id}})</h5>
+        <span class="badge badge-pill badge-primary" v-for="category in post.categories" :key="category">{{category}}</span>
         <p class="card-text"><span v-html="post.excerpt.rendered"></span></p>
         <p>Grade: {{post.fleschKincaid.grade}}</p>
         <p>Rate: {{post.fleschKincaid.rate}}</p>
@@ -16,7 +14,6 @@
         <p>tokens.url: {{post.frequencyCounts.tokens.url}}</p>
         <p>tokens.symbol: {{post.frequencyCounts.tokens.symbol}}</p>
         <p>tokens.punctuation: {{post.frequencyCounts.tokens.punctuation}}</p>
-
       </div>
     </div>
   </div>
